89c3479b60e330d1330dbb52360e83cd4d3c97ef
alliciarocha <allicia30300@gmail.com>
2025-06-27 15:44:41 -0300

    madeModels

--- src/model/madeModels.ts
+++ src/model/madeModels.ts
@@ -1,6 +1,28 @@
+import { Actor, Requirements, Event} from "./models";
+
 export type Sprint = {
     id: string;
     name: string;
     startDate: Date;
     endDate: Date;
+}
+
+export type UseCase = {
+    actors: Array<Actor>;
+    depends: Array<UseCase>;
+    description: string;
+    events: Array<Event>;
+    id: string;
+    name: string;
+    requirements: Array<Requirements>
+}
+
+export type Event = {
+    id: string;
+    name: string;
+    description: string;
+    action: string;
+    requirements: Array<Requirements>;
+    depends: Array<Event>;
+    performer: Actor
 }
