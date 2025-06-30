2cf6270b9fa98c9039fb2d4698b1c0c1881d28fa
Caio Cesar Oliva Coronel <caio.coronel@estudante.ifes.edu.br>
2025-06-30 20:54:48 -0300

    Inicio da criação do grafo de dependencias

---
+++ src/tests/data/modulos.ts
@@ -0,0 +1,18 @@
+import { ModuleInterface } from "../../model/ProjectModels"
+import { actors1, actors2 } from "./actors"
+import { package1 } from "./packages"
+import { requiriments } from "./requisitos"
+import { uc1 } from "./usecases"
+
+
+export const module1: ModuleInterface = {
+    name: "Módulo 1",
+    description: "Descrição do Módulo 1",
+    miniwolrd: "Minimundo do módulo",
+    purpose: "Propósito do Módulo",
+    requisites: requiriments,
+    packages: [package1],
+    actors: [actors1, actors2],
+    useCases: [uc1]
+}
+
---
+++ src/tests/data/usecases.ts
@@ -0,0 +1,14 @@
+import { UseCaseType } from "../../model/madeModels";
+import { actors1 } from "./actors";
+
+
+export const uc1: UseCaseType = {
+    actors: [actors1],
+    depends: [],
+    description: "Descrição do Caso de Uso",
+    events: [],
+    identifier: "TESTE",
+    name: "Algum Nome Aí",
+    requirements: []
+}
+
