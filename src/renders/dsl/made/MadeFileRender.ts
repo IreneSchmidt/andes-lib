c128c2c3c84c141fa51ac77a4d219e3a4b30e4be
chevitaresebruno <chevitarese.bruno@gmail.com>
2025-07-01 20:41:26 -0300

    Checkpoint

---
+++ src/renders/dsl/made/MadeFileRender.ts
@@ -0,0 +1,16 @@
+import { ProjectModuleType } from "../../../model/andes/ProjectTypes";
+import IRender from "../../IRender";
+import MadeProjectRender from "../../made/MadeProjectRender";
+
+
+export default class MadeFileRender implements IRender
+{
+    private module: MadeProjectRender;
+    private
+
+    public render(identationStartLevel: number = 0): string
+    {
+        
+    }
+}
+
