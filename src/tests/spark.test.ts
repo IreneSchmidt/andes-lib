24bd09e2abe654f5027f0f77f85e00097384385d
chevitaresebruno <chevitarese.bruno@gmail.com>
2025-07-01 14:46:57 -0300

    Fix: corrigindo o spark.test.ts para testar todos os geradores do spark

--- src/tests/spark.test.ts
+++ src/tests/spark.test.ts
@@ -8,18 +8,53 @@ import { normalize } from "path";
 import createFolderAndFile from "../application/IO.ts"
 
 import SparkFileRender from "../renders/dsl/spark/SparkFileRender.ts"
+import SparkConfigRender from '../renders/dsl/spark/SparkConfigRender.ts';
+import SparkEnumEntityRender from '../renders/dsl/spark/SparkEnumEntityRender.ts';
+import SparkEntityRender from '../renders/dsl/spark/SparkEntytiRender.ts';
+import SparkPackageRender from '../renders/dsl/spark/SparkPackageRender.ts';
 
 
-describe("Generate Spark", ()=> {
-    
-    let sparkFR = new SparkFileRender(project);
+function testConfig()
+{
+    const config = new SparkConfigRender(project.overview);
+    expect(config.render()).toBe(`\nConfiguration  {\n\tsoftware_name: "Projeto de Teste"\n\tabout: "Descrição do meu projeto de Teste"\n\tlanguage: arquitetura-solicitada-pelo-usuário\n}`);
+}
 
-    createFolderAndFile("out/tests/spark", "spark.spark", sparkFR.render());
+function testEnum()
+{
+    const enume = project.modules[0].packages[0].enums?.at(0);
+    if(enume == undefined)
+        { return; }
 
-    let expectContent = readFileSync("./src/tests/expect/test.spark", "utf-8");
-    it("Generate Spark file content", () => {
-        // expect(normalize(sparkFR.render())).toEqual(normalize(expectContent))
-        // por algum motivo tá dando erro na comparação, mas o arquivo é gerado certinho
-    })
-});
+    const e = new SparkEnumEntityRender(enume);
+
+    expect(e.render()).toBe("\nenum Semestre {\n\tPRIMEIRO\n\tSEGUNDO\n}")
+}
+
+function testEntity()
+{
+    const _aluno = project.modules[0].packages[0].entities[2];
+
+    const aluno = new SparkEntityRender(_aluno);
+
+    expect(aluno.render()).toBe("\nentity Aluno {\n\tnome: string\n\tSemestreIngresso uses Semestre\n\tmatricula OneToOne Matricula\n}")
+}
+
+function testPackage()
+{
+    const _pgk = project.modules[0].packages[0];
+
+    const pkg = new SparkPackageRender(_pgk);
+
+    expect(pkg.render()).toBe(`\nmodule Pacote1 {\n\tentity Escola {\n\t\tnome: string\n\t}\n\tentity Matricula {\n\t\tcodigo: string\n\t\tescola ManyToOne Escola\n\t}\n\tentity Aluno {\n\t\tnome: string\n\t\tSemestreIngresso uses Semestre\n\t\tmatricula OneToOne Matricula\n\t}\n\tenum Semestre {\n\t\tPRIMEIRO\n\t\tSEGUNDO\n\t}\n}`);
+}
+
+test("Generate Spark", ()=>
+    {
+        testConfig();
+        testEnum();
+        testEntity();
+        testPackage();
+    }
+);
 
